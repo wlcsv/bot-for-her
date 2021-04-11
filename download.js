e8342465efceec514261fcde534d8c84b3d19805
wallacy <wallacyvieirada89@gmail.com>
2021-04-11 19:52:07 +0000

    first commit sem ser o first commit pq eu não consigo pensar em nada

--- download.js
+++ download.js
@@ -2,3 +2,15 @@ var fs = require('fs');
 var request = require('request');
 var readlineSync = require('readline-sync');
 
+var url = 'aaaaa'
+var filename = 'aaaaaaaa'
+
+var download = function(url, filename, callback) {
+    request.head(url, function(err, res, body) {
+        request(url).pipe(fs.createWriteStream(filename).on('close', callback));
+    });
+};
+
+download(url, filename, function() {
+    console.log("done!");
+})
